15dbd2555075c2da364f28a5453f14b97976c3ba
Son Tran <son.tran@team.enouvo.com>
2022-07-20 16:02:51 +0700

    feat(release): update lint

--- dist/index.js
+++ dist/index.js
@@ -80,9 +80,11 @@ module.exports = {
     'no-param-reassign': 'error',
     'prefer-destructuring': ['error'],
     'object-shorthand': 'error',
+    'dot-notation': 'error',
     'multiline-comment-style': ['error', 'starred-block'],
     'prefer-template': 'error',
     'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    'default-case-last': 'error',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
